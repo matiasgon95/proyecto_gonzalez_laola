c1ee61df7b3ef3c620007d4ad3fb8464c069254f
matiasgon95 <matiasegonzalez@gmail.com>
2025-05-09 16:28:50 -0300

    coloco nombre de la BD

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'DB_gonzalez_laola',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
