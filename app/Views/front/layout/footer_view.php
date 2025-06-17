e2146d60a00bd903e78021dd66f25a14a7694236
matiasgon95 <matiasegonzalez95@gmail.com>
2025-06-17 20:13:36 -0300

    comentarios en footer_view

--- app/Views/front/layout/footer_view.php
+++ app/Views/front/layout/footer_view.php
@@ -1,11 +1,13 @@
-<!-- Footer -->
+<!-- Footer - Pie de página principal del sitio -->
 <footer class="footer-custom">
     <div class="container py-4">
         <div class="row g-4">
+            <!-- Columna de información de la empresa -->
             <div class="col-lg-4 col-md-6">
                 <h5 class="footer-heading">GL Technology</h5>
                 <p class="footer-text">Tu tienda de confianza en componentes y hardware para computadoras.</p>
             </div>
+            <!-- Columna de enlaces rápidos a secciones principales -->
             <div class="col-lg-4 col-md-6">
                 <h5 class="footer-heading">Enlaces Rápidos</h5>
                 <ul class="footer-links">
@@ -16,9 +18,11 @@
                     <li><a href="<?= base_url('terminos') ?>" class="terms-link">Términos y Condiciones</a></li>
                 </ul>
             </div>
+            <!-- Columna de redes sociales -->
             <div class="col-lg-4 text-center">
                 <h5 class="footer-heading">SÍGUENOS</h5>
                 <div class="footer-social-wrapper">
+                <!-- Iconos de redes sociales con enlaces externos -->
                 <div class="footer-social">
                   <a href="https://www.facebook.com" class="social-icon" target="_blank"><i class="fab fa-facebook-f"></i></a>
                   <a href="https://www.instagram.com" class="social-icon" target="_blank"><i class="fab fa-instagram"></i></a>
@@ -28,6 +32,7 @@
                 </div>
             </div>
         </div>
+        <!-- Sección inferior con copyright -->
         <div class="footer-bottom mt-4 pt-3 border-top">
             <p class="mb-0 text-center">© 2025 González Matías - Laola Mariano. Todos los derechos reservados.</p>
         </div>
