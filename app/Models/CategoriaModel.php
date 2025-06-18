2e6951843ea0ba295bb1165b98b2397656a82930
matiasgon95 <matiasegonzalez95@gmail.com>
2025-06-17 22:32:15 -0300

    documentacion CategoriaModel

--- app/Models/CategoriaModel.php
+++ app/Models/CategoriaModel.php
@@ -4,10 +4,25 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
+/**
+ * Modelo para gestionar las categorías de productos
+ * 
+ * Este modelo maneja las operaciones CRUD para las categorías en la base de datos
+ */
 class CategoriaModel extends Model
 {
+    /**
+     * Nombre de la tabla en la base de datos
+     */
     protected $table = 'categorias';
+    
+    /**
+     * Clave primaria de la tabla
+     */
     protected $primaryKey = 'id';
 
+    /**
+     * Campos que se pueden modificar en la base de datos
+     */
     protected $allowedFields = ['descripcion', 'activo', 'created_at', 'updated_at'];
 }
